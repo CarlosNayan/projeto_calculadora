aa9b19cbfae3f5a58542a528d9b2a4545d8b56ed
Carlos Nayan <119333088+CarlosNayan@users.noreply.github.com>
2022-12-26 11:43:20 -0300

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -19,6 +19,6 @@ function calcular() {
   if (resultado) {
     document.getElementById("resultado").innerHTML = eval(resultado);
   } else {
-    document.getElementById("resultado").innerHTML = "0";
+    document.getElementById("resultado").innerHTML = "N/A";
   }
 }
